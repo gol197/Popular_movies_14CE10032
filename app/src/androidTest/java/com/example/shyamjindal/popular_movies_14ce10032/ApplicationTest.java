0ddc0daaaa87044721c264f206015f2d1a1acb11
unknown <mayank.jindal5@gmail.com>
2015-06-21 14:42:01 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/shyamjindal/popular_movies_14ce10032/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.shyamjindal.popular_movies_14ce10032;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
